d44da51a39cdcfb48465d0028d6d0fe8b49d961d
abhinav-1312 <abhinav@dkgrouplabs.com>
2024-03-15 05:45:16 +0530

    added transaction summary page

--- src/pages/ohq/ohq.jsx
+++ src/pages/ohq/ohq.jsx
@@ -110,7 +110,6 @@ const Ohq = () => {
         placeholder="Search item"
         allowClear
         enterButton="Search"
-        
         size="large"
         onSearch={(e) => handleSearch(e.target.value)}
         onChange={(e) => handleSearch(e.target.value)}
