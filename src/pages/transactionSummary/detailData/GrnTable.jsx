ab4144c4bab269cae87e79c8f20c5f0e7411db35
abhinav-1312 <abhinav@dkgrouplabs.com>
2024-08-06 12:00:38 +0530

    grn

--- src/pages/transactionSummary/detailData/GrnTable.jsx
+++ src/pages/transactionSummary/detailData/GrnTable.jsx
@@ -214,7 +214,8 @@ const GrnTable = ({type, data, itemList}) => {
         }
     ]
   return (
-    <DetailData dataColumn={type === "PO" ? [...dataColumn,...poExtraColumns, ] :(type === "IRP" ? [...dataColumn, ...consumerDetails, ...irpExtraColumn, ...orgConsignorDetails] : [...dataColumn, ...orgConsignorDetails, ...orgConsigneeDetails] )} itemListColumn={type === "PO" ? [...itemListColumn, {title: "Unit Price", dataIndex: "unitPrice"}] : {itemListColumn}} data={data} itemList={itemList}/>
+    // <DetailData dataColumn={type === "PO" ? [...dataColumn,...poExtraColumns ] :(type === "IRP" ? [...dataColumn, ...consumerDetails, ...irpExtraColumn, ...orgConsignorDetails] : [...dataColumn] )} itemListColumn={type === "PO" ? [...itemListColumn] : [...itemListColumn]} data={data} itemList={itemList}/>
+    <DetailData dataColumn={type === "PO" ? [...dataColumn,...poExtraColumns, ] :(type === "IRP" ? [...dataColumn, ...consumerDetails, ...irpExtraColumn, ...orgConsignorDetails] : [...dataColumn, ...orgConsignorDetails, ...orgConsigneeDetails] )} itemListColumn={type === "PO" ? [...itemListColumn, {title: "Unit Price", dataIndex: "unitPrice"}] : itemListColumn} data={data} itemList={itemList}/>
   )
 }
 
