4e584a96dcee26fe453fffb9e2796832ecf2a19b
abhinav-1312 <abhinav@dkgrouplabs.com>
2024-10-27 14:52:13 +0530

    formcontainer

--- src/components/FormContainer.jsx
+++ src/components/FormContainer.jsx
@@ -1,24 +1,13 @@
-import React, { useRef } from "react";
-import ButtonContainer from "./ButtonContainer";
-import { useReactToPrint } from "react-to-print";
-import { message } from "antd";
+import React from "react";
 
-const FormContainer = ({ children, onFinish, submitBtnEnabled, printBtnEnabled }) => {
-  const ref = useRef()
-  const handlePrint = useReactToPrint({
-    content: () => ref.current,
-    documentTitle: 'SAI_Form',
-});
+const FormContainer = React.forwardRef((props, ref) => {
+  const {children } = props
 
   return (
     <div className="a4-container" ref={ref}>
       {children}
-
-      <div style={{ marginTop: "1rem" }}>
-        <ButtonContainer handlePrint={handlePrint} onFinish={onFinish} submitBtnEnabled={submitBtnEnabled} printBtnEnabled={printBtnEnabled} />
-      </div>
     </div>
   );
-};
+});
 
 export default FormContainer;
